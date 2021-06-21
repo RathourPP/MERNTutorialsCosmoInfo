100a4aca2b381357f9ba30f37bfdf0f17b763d17
discover.rathore123@gmail.com <discover.rathore123@gmail.com>
2021-06-21 10:43:36 +0530

    Searching and Deleting Employee

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './styles/style.css'
 import Header from './Component/Common/Header'
 import Content from './Component/Common/Content'
 import Footer from './Component/Common/Footer'
+import User from './Component/UserInfo'
 
 export default()=>{
  return(
   <>
-  <Header/>
-  <Content/>
-  <Footer/>
+  <User/>
   </>
  )
 }
--- src/Component/UserInfo.js
+++ src/Component/UserInfo.js
@@ -1,43 +1,89 @@
-export default({employeeList})=>{
-    return(
-        <div className="table-responsive"> 
-        {
-          employeeList && employeeList.map((item,index)=>{
-            return(
-              <table  className="table table-bordred table-striped" key={index}>
-              <thead>
-                <tr key={index}>
-                     <th>User Id</th>
-                     <th>Name</th>
-                     <th>Email</th>
-                     <th>Contact</th>
-                     <th>Pincode</th>
-                </tr>
-                     </thead>
-                  <tbody>
-                  
-                  <tr key={index}>
-                
-                      <td>{item.UserId}</td>
-                      <td>{item.Name}</td>
-                      <td>{item.Email}</td>
-                      <td>{item.Contact}</td>
-                      <td>{item.Pincode}</td>
+import { useState } from "react"
+import { Input,Button } from "reactstrap"
+import '../styles/list.css'
+
+export default()=>{
+
+  const[employee,setEmployee]=useState([
+    {id:1,name:"Prabal Pratap Singh",city:"Lucknow",state:"Uttar Pradesh"},
+    {id:2,name:"Yashwendra Pratap Singh",city:"Ayodhya",state:"Uttar Pradesh"},
+    {id:3,name:"Mohit",city:"Faizabad",state:"Uttar Pradesh"},
+    {id:4,name:"Saurabh",city:"Raebareli",state:"Uttar Pradesh"}
+  ])
+
+  const[message,setMessage]=useState('');
+
+  const[name,setName]=useState('');
+
+  const searchHandler=(e)=>{
+      setName(e.target.value);
+  }
+
+  const deleteEmployeeHandler=(id)=>{
+    const newEmployee=employee.filter((item)=>item.id!==id);
+    setEmployee(newEmployee);
+    setMessage(`You Deleted Record With Id = ${id}`);
+  }
+
+  const searchOnClick=(name)=>{
+    const newEmployee=employee.filter((item)=>item.name===name);
+    setEmployee(newEmployee);
+  }
+
+  const clearSearch=()=>{
+    const newEmployee=[
+      {id:1,name:"Prabal Pratap Singh",city:"Lucknow",state:"Uttar Pradesh"},
+      {id:2,name:"Yashwendra Pratap Singh",city:"Ayodhya",state:"Uttar Pradesh"},
+      {id:3,name:"Mohit",city:"Faizabad",state:"Uttar Pradesh"},
+      {id:4,name:"Saurabh",city:"Raebareli",state:"Uttar Pradesh"}
+    ]
+    setEmployee(newEmployee);
+    setName('');
+  }
+
+ return(
+  <div className="container">
+  <div className="row">
+      <div className="col-md-8">
+          <div className="panel panel-primary">
+              <div className="panel-heading">
+               <Input placeholder="Search" value={name} onChange={searchHandler}/>
+               <Button onClick={()=>searchOnClick(name)}>Search</Button>
+               <button onClick={()=>clearSearch()}>Clear</button>
+              </div>
+              <div className="panel-body">
+                  <ul className="list-group">
+                      <li className="list-group-item">
+                          {
+                            employee && employee.map((item,index)=>{
+                              return(
+                              <table key={index}>
+                                <tbody>
+                                  <tr>
+                                    <td>{item.name}</td>
+                                    <td style={{width:"5px"}}></td>
+                                    <td><span style={{color:"red",cursor:"pointer"}} onClick={()=>deleteEmployeeHandler(item.id)}>Delete</span></td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              )
+                            })
+                          }
+                      </li>
+                  </ul>
+              </div>
+              <div className="panel-footer">
+                  <div className="row">
+                      <div className="col-md-6">
+                          <h6>
+                             <span className="label label-info">{message}</span></h6>
+                      </div>
                      
-                  </tr>
-                  
-                 
-                  </tbody>
-        
-</table>
-            )
-          })
-        }
-             
-
-<div className="clearfix"></div>
-
-                
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
-    )
+</div>
+ )
 }
